cb8ef5e875108e6700e1da397a720e621d9f2164
Dougherty, Joe <josepd@wharton.upenn.edu>
2015-04-21 11:48:55 -0400

    add end-user docs in the form of code

---
+++ usage.py
@@ -0,0 +1,9 @@
+from wrds import DataTeam
+
+developer = DataTeam(team_member='joe')
+
+# Please press 'D' to get my attention if 
+# I happen to have headphones on -- Thanks!
+
+developer.request_assistance()
+
